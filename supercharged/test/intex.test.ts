c64761418b81cccab04d6ada280d424ca2f7208b
Alexander Baygeldin <a.baygeldin@gmail.com>
2023-08-30 03:29:27 +0100

    add tmp test

--- supercharged/test/intex.test.ts
+++ supercharged/test/intex.test.ts
@@ -28,4 +28,25 @@ describe('worker', () => {
       expect(await res.text()).toContain('Geolocation: Hello World!')
     })
   })
+
+  describe('when accessing /', () => {
+    xit('returns a translated hello world message', async () => {
+      const path = '/'
+      const body = '<html><span id="caption"></span></html>'
+
+      // We make two requests in the tests, so we mock subrequests to the homepage twice:
+      expect({ url: `${env.HOMEPAGE_URL}${path}` })
+        .toBeFetchedAndReturnTimes({ body }, 2)
+
+      // But we only expect the hello API to be called once because we cache the response:
+      expect({ url: `${env.HELLO_API_URL}/?cc=GB` })
+        .toBeFetchedAndReturn({ body: '{"code":"gb","hello":"Hello"}' })
+
+      const res = await request(path)
+      const anotherRes = await request(path)
+
+      expect(await res.text()).toContain('ｈｅｌｌｏ')
+      expect(await anotherRes.text()).toContain('ｈｅｌｌｏ')
+    })
+  })
 })
